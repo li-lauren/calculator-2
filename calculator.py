328690af60bf3e7479ec855999a8652064d2fcfb
li-lauren <laurenlidev@gmail.com>
2020-10-01 01:44:44 +0000

    Finalized calculator.py

--- calculator.py
+++ calculator.py
@@ -3,5 +3,42 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+while True:
+    exp = input("Enter an expression: ")
+    exp_tokens = exp.split(' ')
+    if exp_tokens[0] == "q" or exp_tokens[0] == "Q":
+        break
+    else:
+        for index in range(1, len(exp_tokens)):
+            exp_tokens[index] = int(exp_tokens[index])
+            
+        if exp_tokens[0] == "+":
+            answer = (add(exp_tokens[1], exp_tokens[2]))
 
-# Replace this with your code
+        elif exp_tokens[0] == "-":
+            answer = subtract(exp_tokens[1], exp_tokens[2])
+
+        elif exp_tokens[0] == "*":
+            answer = multiply(exp_tokens[1], exp_tokens[2])
+        
+        elif exp_tokens[0] == "/":
+            answer = divide(exp_tokens[1], exp_tokens[2])
+
+        elif exp_tokens[0] == "square":
+            answer = square(exp_tokens[1])
+
+        elif exp_tokens[0] == "cube":
+            answer = cube(exp_tokens[1])
+
+        elif exp_tokens[0] == "pow":
+            answer = power(exp_tokens[1], exp_tokens[2])
+
+        elif exp_tokens[0] == "mod":
+            answer = mod(exp_tokens[1], exp_tokens[2])
+
+        else:
+            answer = 'Invalid expression.'
+
+    print(answer)
+
+
